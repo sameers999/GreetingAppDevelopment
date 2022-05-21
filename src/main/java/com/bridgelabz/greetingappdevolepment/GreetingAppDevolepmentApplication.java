a5ba3fb8572080fe24be1413c9e8f0e68fcf35db
Syedsameer <syedmahammadsameer999@gmail.com>
2022-05-21 12:33:17 +0530

    Added: Creating Greeting App Devepment Controller

---
+++ src/main/java/com/bridgelabz/greetingappdevolepment/GreetingAppDevolepmentApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingappdevolepment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppDevolepmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingAppDevolepmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bridgelabz/greetingappdevolepment/GreetingAppDevolepmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingappdevolepment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppDevolepmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
